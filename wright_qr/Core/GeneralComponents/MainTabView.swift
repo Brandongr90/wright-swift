f1b1ee039d2d1770f57aa459ae86f35d090bb585
Brandongr90 <l.gorb010920@itses.edu.mx>
2024-12-04 14:05:15 -0600

    TabBar Implementation

---
+++ wright_qr/Core/GeneralComponents/MainTabView.swift
@@ -0,0 +1,54 @@
+//
+//  MainTabView.swift
+//  wright_qr
+//
+//  Created by Brandon Gonzalez on 04/12/24.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    @StateObject private var userManager = UserManager.shared
+    @State private var selectedTab = 0
+    
+    var body: some View {
+        TabView(selection: $selectedTab) {
+            NavigationStack {
+                HomeView()
+            }
+            .tabItem {
+                Image(systemName: "house.fill")
+                Text("Home")
+            }
+            .tag(0)
+            
+            NavigationStack {
+                GenerateQRView()
+            }
+            .tabItem {
+                Image(systemName: "qrcode")
+                Text("Generar")
+            }
+            .tag(1)
+            
+            NavigationStack {
+                ScanQRView()
+            }
+            .tabItem {
+                Image(systemName: "qrcode.viewfinder")
+                Text("Escanear")
+            }
+            .tag(2)
+            
+            NavigationStack {
+                ProfileView()
+            }
+            .tabItem {
+                Image(systemName: "person.fill")
+                Text("Perfil")
+            }
+            .tag(3)
+        }
+        .accentColor(Color.main)
+    }
+}
---
+++ wright_qr/Features/Profile/ProfileView.swift
@@ -0,0 +1,14 @@
+//
+//  ProfileView.swift
+//  wright_qr
+//
+//  Created by Brandon Gonzalez on 04/12/24.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
